8f428fe53c58f9e3aba0d9cc13fb1e8be8d88b13
A <105343395+Map-Jo@users.noreply.github.com>
2022-08-02 22:47:39 +0900

    Create recommend.py

---
+++ recommend.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from kmr_dataset import load_rates
+from kmr_dataset import get_paths
+import koreanize_matplotlib
+paths = get_paths(size='small')
